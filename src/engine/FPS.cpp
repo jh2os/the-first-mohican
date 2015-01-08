19bba94c09aedb4d8719a877fd480b04f5899a12
jh2os <jh2os24@gmail.com>
2015-01-08 00:37:17 -0500

    applided fps movement speed factor

--- src/engine/FPS.cpp
+++ src/engine/FPS.cpp
@@ -4,28 +4,29 @@ FPS::FPS() {
 
 	oldTime = 0;
 	lastTime = 0;
-	speedFactor = 0;
-	frames = 0;
-	numFrames = 0;
+	speedFactor = 0.0;
+	fps = 0;
 }
 
 void FPS::OnLoop() {
 	unsigned int currentTick = SDL_GetTicks();
-	if( oldTime + 1000 < currentTick) {
-		oldTime = currentTick;
-		numFrames = frames;
-		frames = 0;
-	}
-
-	speedFactor = ((currentTick - lastTime) / 1000.0f) * 64.0f;
+	speedFactor = ((currentTick - lastTime) / 1000.0f);
+	fps = 1 / speedFactor;
 	lastTime = currentTick;
-	frames++;
 }
 
 int FPS::GetFPS() {
-	return numFrames;
+	return fps;
 }
 
 float FPS::GetSpeedFactor() {
 	return speedFactor;
+}
+
+float FPS::GetSpeedFactor(int distance) {
+	return speedFactor * distance;
+}
+
+float FPS::GetSpeedFactor(float distance) {
+	return speedFactor * distance;
 }
